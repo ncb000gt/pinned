92ab45e80ea9d186823b8f04a0f69a5a6a372532
Nick Campbell <nicholas.j.campbell@gmail.com>
2012-01-08 00:56:08 -0500

    Added really rudimentary authentication. Keys still don't require it yet. Updated a handful of other things and removed some old/excess code.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 var express = require('express'),
+    fs = require('fs'),
+    bcrypt = require('bcrypt'),
+    config = require('./config'),
     db = require('./lib/db')();
 
 var app = module.exports = express.createServer();
 
 app.configure(function(){
+  app.set('view engine', 'jade');
   app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: "say WUUUUT?!?"
+  }));
   app.use(app.router);
 });
 
-app.options('/:key', function(req, res) {
+app.options('/keys/:key', function(req, res) {
   res.send("", {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "PUT", "Access-Control-Allow-Headers": "Origin, Content-Type"}, 200);
 });
 
-app.put('/:key', function(req, res) {
+app.put('/keys/:key', function(req, res) {
   db.add(req.params.key);
   res.send("", {"Access-Control-Allow-Origin": "*"}, 200);
 });
 
-app.get('/', function(req, res){
+app.get('/keys', function(req, res){
   db.getAll(null, function(err, keys) {
     res.json(keys);
   });
+});
+
+app.get('/', function(req, res){
+  if (!(req.session && req.session.authed)) {
+    res.render('login', {
+    });
+  } else {
+    var bookmarklet = fs.readFileSync('./bookmarklet.js');
+    res.render('home', {
+      bookmarklet: bookmarklet,
+      pinned: [{url: '/test'}]
+    });
+  }
+});
+
+app.post('/', function(req, res) {
+  if (req.body && req.body.pw) {
+    var pw = req.body.pw;
+    if (bcrypt.compare_sync(pw, config.password)) {
+      req.session.authed = true;
+    }
+  }
+
+  res.redirect('/');
 });
 
 app.listen(process.env.NODE_PORT || (process.env.NODE_ENV === 'production' ? 80 : 8000)); 
