71c4261a2d4a0fac9b9f23bf19d2920167a42a9b
Nick Campbell <nicholas.j.campbell@gmail.com>
2012-01-08 01:48:03 -0500

    views
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express'),
 var app = module.exports = express.createServer();
 
 app.configure(function(){
+  app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
